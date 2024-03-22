40169451bfed40b2d59736cda7a6b2e883991af6
Maggie <mageinblack.x@gmail.com>
2024-03-22 11:45:34 +0300

    Updated neovim tab settings

--- init.lua
+++ init.lua
@@ -5,3 +5,10 @@ require("core.lazy")
 -- Set line numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- Tabs
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
